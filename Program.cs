c043c7593858fc714957acc8674d9948bb6f3a6d
Ahmed al Lakani <ahmed.al.lakani@gmail.com>
2021-03-13 02:43:26 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Activities.XamlIntegration;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TestCoreW
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var helloWorldActivity = new System.Activities.Statements.Sequence()
+            //{
+            //    Activities =
+            //    {
+            //        new WriteLine
+            //        {
+            //            Text = "Hello World! CoreWF" 
+            //        } , 
+            //        new MessageBox 
+            //        { 
+            //            Text = "new Activity"
+            //        }
+            //    }
+            //};
+
+            //Console.WriteLine("Hello World!");
+            //System.Activities.WorkflowInvoker.Invoke(helloWorldActivity);
+
+            //var helloWorldActivity = ActivityXamlServices.Load(new StringReader(xamlString));
+            //System.Activities.WorkflowInvoker.Invoke(helloWorldActivity);
+            string xamlString;
+            
+            ActivityXamlServicesSettings settings = new ActivityXamlServicesSettings
+            {
+                CompileExpressions = true
+            };
+
+        
+        xamlString = System.IO.File.ReadAllText("D:\\Projects\\TestCoreW\\WorkflowConsoleApplication1\\Workflow1.xaml");
+               
+            var ActivityFromFile = ActivityXamlServices.Load(new StringReader(xamlString), settings);
+            Dictionary<string, object> inputs = new Dictionary<string, object>();
+
+            inputs.Add("x", 50);
+            inputs.Add("Path", "C:\\Work\\BUGS\\INC000000286049_Wrong_Portfolio_Position\\");
+
+            var Ret = System.Activities.WorkflowInvoker.Invoke(ActivityFromFile, inputs);
+
+            System.Console.WriteLine(Ret["Out"]);
+            //System.Console.ReadLine();
+            //Console.ReadLine();
+
+        }
+    }
+}
---
+++ WorkflowConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WorkflowConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity workflow1 = new Workflow1();
+            Dictionary<string, object> inputs = new Dictionary<string, object>();
+
+            inputs.Add("x", 50);
+            inputs.Add("Path", "C:\\Work\\BUGS\\INC000000286049_Wrong_Portfolio_Position\\");
+
+            var Ret = WorkflowInvoker.Invoke(workflow1, inputs);
+
+            System.Console.WriteLine(Ret["Out"]);
+            System.Console.ReadLine();
+        }
+    }
+}
